f7e25cecd3d42278c0a764c500c46c969586585b
Andrea Grandi <a.grandi@gmail.com>
2015-02-14 13:29:26 +0000

    Initial structure of the application

---
+++ booking/restaurants/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Restaurant(models):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    opening_time = models.IntegerField()
+    closing_time = models.IntegerField()
+
+
+class Table(models):
+    restaurant = models.ForeignKey(Restaurant)
+    size = models.IntegerField()
+
+
+class Booking(models):
+    table = models.ForeignKey(Table)
+    people = models.IntegerField()
+    booking_date_time = models.DateTimeField()
