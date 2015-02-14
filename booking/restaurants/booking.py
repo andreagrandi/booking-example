88201abf1d38d41ad7a8fdcfa6b34634a3557d7f
Andrea Grandi <a.grandi@gmail.com>
2015-02-14 18:32:28 +0000

    Add method to get first smaller table available and implement two unit tests

---
+++ booking/restaurants/booking.py
@@ -0,0 +1,24 @@
+from datetime import timedelta
+from .models import Table, Booking
+
+def book_restaurant_table(restaurant, people, booking_date_time):
+    pass
+
+def get_first_table_available(restaurant, booking_date_time, people):
+    delta = timedelta(seconds=60*90)  # 1,5 hours time delta
+    l_bound_time = booking_date_time - delta
+    u_bound_time = booking_date_time + delta
+
+    tables_booked = Booking.objects.filter(table__restaurant=restaurant,
+        booking_date_time__gt=l_bound_time, booking_date_time__lt=u_bound_time).values('table')
+    tables_booked_ids = [x['table'] for x in tables_booked]
+
+    tables = Table.objects.filter(restaurant=restaurant,
+        restaurant__opening_time__lte=booking_date_time.hour,
+        restaurant__closing_time__gte=booking_date_time.hour+1.5,
+        size__gte=people).exclude(id__in=tables_booked_ids).order_by('size')
+
+    if tables.count() == 0:
+        return None
+    else:
+        return tables[0]
---
+++ booking/restaurants/tests/test_booking.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from datetime import datetime
+from .factories import RestaurantFactory, TableFactory, BookingFactory
+from restaurants import booking
+from restaurants.models import Booking
+
+class ModelsTest(TestCase):
+    def setUp(self):
+        self.restaurant_1 = RestaurantFactory.create(opening_time=18, closing_time=23)
+        self.restaurant_1_table_1 = TableFactory.create(restaurant=self.restaurant_1, size=2)
+        self.restaurant_1_table_2 = TableFactory.create(restaurant=self.restaurant_1, size=4)
+        self.booking_1 = BookingFactory.create(
+            table=self.restaurant_1_table_2,
+            people=4,
+            booking_date_time=datetime(2015, 2, 14, 19, 0))
+
+    def test_get_first_table_available(self):
+        table = booking.get_first_table_available(
+            restaurant=self.restaurant_1,
+            booking_date_time=datetime(2015, 2, 14, 20, 0),
+            people=2)
+        self.assertEqual(table.id, self.restaurant_1_table_1.id)
+
+    def test_get_first_table_available_unavailable(self):
+        table = booking.get_first_table_available(
+            restaurant=self.restaurant_1,
+            booking_date_time=datetime(2015, 2, 14, 20, 0),
+            people=4)
+        self.assertEqual(table, None)
