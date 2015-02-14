0e19e8fdbe92000de56de7badbef8625c97c1877
Andrea Grandi <a.grandi@gmail.com>
2015-02-14 23:50:49 +0000

    Add tests for booking after closing time and before opening time

--- booking/restaurants/tests/test_booking.py
+++ booking/restaurants/tests/test_booking.py
@@ -27,3 +27,17 @@ class ModelsTest(TestCase):
             booking_date_time=datetime(2015, 2, 14, 20, 0),
             people=4)
         self.assertEqual(table, None)
+
+    def test_unavailable_tables_1_hour_before_closing(self):
+        table = booking.get_first_table_available(
+            restaurant=self.restaurant_1,
+            booking_date_time=datetime(2015, 2, 14, 22, 0),
+            people=2)
+        self.assertEqual(table, None)
+
+    def test_unavailable_tables_1_hour_before_opening(self):
+        table = booking.get_first_table_available(
+            restaurant=self.restaurant_1,
+            booking_date_time=datetime(2015, 2, 14, 17, 0),
+            people=2)
+        self.assertEqual(table, None)
