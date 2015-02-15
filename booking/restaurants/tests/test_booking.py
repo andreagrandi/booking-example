c436fc179f706bd077ca89ae904ff07ce97e09de
Andrea Grandi <a.grandi@gmail.com>
2015-02-15 11:33:40 +0000

    Make tests timezone aware

--- booking/restaurants/tests/test_booking.py
+++ booking/restaurants/tests/test_booking.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from datetime import datetime
+import pytz
 from .factories import RestaurantFactory, TableFactory, BookingFactory
 from restaurants import booking
 from restaurants.models import Booking
@@ -12,32 +13,32 @@ class ModelsTest(TestCase):
         self.booking_1 = BookingFactory.create(
             table=self.restaurant_1_table_2,
             people=4,
-            booking_date_time=datetime(2015, 2, 14, 19, 0))
+            booking_date_time=datetime(2015, 2, 14, 19, 0, tzinfo=pytz.UTC))
 
     def test_get_first_table_available(self):
         table = booking.get_first_table_available(
             restaurant=self.restaurant_1,
-            booking_date_time=datetime(2015, 2, 14, 20, 0),
+            booking_date_time=datetime(2015, 2, 14, 20, 0, tzinfo=pytz.UTC),
             people=2)
         self.assertEqual(table.id, self.restaurant_1_table_1.id)
 
     def test_get_first_table_available_unavailable(self):
         table = booking.get_first_table_available(
             restaurant=self.restaurant_1,
-            booking_date_time=datetime(2015, 2, 14, 20, 0),
+            booking_date_time=datetime(2015, 2, 14, 20, 0, tzinfo=pytz.UTC),
             people=4)
         self.assertEqual(table, None)
 
     def test_unavailable_tables_1_hour_before_closing(self):
         table = booking.get_first_table_available(
             restaurant=self.restaurant_1,
-            booking_date_time=datetime(2015, 2, 14, 22, 0),
+            booking_date_time=datetime(2015, 2, 14, 22, 0, tzinfo=pytz.UTC),
             people=2)
         self.assertEqual(table, None)
 
     def test_unavailable_tables_1_hour_before_opening(self):
         table = booking.get_first_table_available(
             restaurant=self.restaurant_1,
-            booking_date_time=datetime(2015, 2, 14, 17, 0),
+            booking_date_time=datetime(2015, 2, 14, 17, 0, tzinfo=pytz.UTC),
             people=2)
         self.assertEqual(table, None)
